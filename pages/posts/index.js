7f9869983a4195d4104afbbb7e5edfd327973557
Hernani <hernani.luiz@gmail.com>
2020-06-21 18:07:32 -0300

    ajuste posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Layout from '../../components/layout/Layout';
+import { attributes as postAttributes } from '../../content/posts.md';
 
 const Posts = () => {
-  return <Layout title="aqui e posts">posts</Layout>;
+  const { posts } = postAttributes;
+  return (
+    <Layout title="aqui e posts">
+      <Posts posts={posts} />
+    </Layout>
+  );
 };
 
 export default Posts;
