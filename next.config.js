f28df961a716d0b06e11ba2ae03086b0dedb3c8c
Hernani <hernani.luiz@gmail.com>
2020-06-21 17:40:39 -0300

    remove css config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,11 @@
 const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
-module.exports = withCSS(
-  withSass({
-    webpack: (cfg) => {
-      cfg.module.rules.push(
-        {
-          test: /\.md$/,
-          loader: 'frontmatter-markdown-loader',
-          options: { mode: ['react-component'] },
-        },
-        {
-          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              //   name: '[name].[ext]',
-            },
-          },
-        }
-      );
-      return cfg;
-    },
-  })
-);
-
-// const withSass = require('@zeit/next-sass');
-// module.exports = withCSS(
-//   withSass({
-//     /* config options here */
-//   })
-// );
+module.exports = withSass({
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
+});
