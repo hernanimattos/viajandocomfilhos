a3c57183d769843401f1054f61563e61e9d8636e
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-28 15:50:26 -0300

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: ['./pages/**/*.js', './components/**/*.js'],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
