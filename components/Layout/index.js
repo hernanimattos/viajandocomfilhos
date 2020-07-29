dfd5a8785adde51621395d3cd0093178907a8481
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-29 20:47:35 -0300

    twitter

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,6 +8,12 @@ const Layout = ({ children, ...props }) => {
 			<Head>
 				<meta name='title' content={props.title} />
 				<meta name='description' content={props.description} />
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:description' content={props.description} />
+				<meta name='twitter:title' content={props.title} />
+				<meta name='twitter:site' content='viajandocomfilhos.com.br' />
+				<meta name='twitter:image' content={props.thumbnail} />
+				<meta name='twitter:creator' content='viajandocomfilhos.com.br' />
 			</Head>
 			<section id='main'>
 				<Header />
